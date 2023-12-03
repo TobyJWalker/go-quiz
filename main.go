58f8751959f0c1792326704e0d2237512e894e5d
TobyJWalker <tobyjwalker03@outlook.com>
2023-12-03 19:09:07 +0000

    open and parse csv

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
+	"fmt"
+	"os"
 )
 
 // create flags
@@ -9,3 +12,29 @@ var (
 	timer = flag.Int("t", 30, "Time limit for the quiz in seconds")
 	csv_path = flag.String("csv", "problems.csv", "Path to the csv file")
 )
+
+func main() {
+
+	// parse flags
+	flag.Parse()
+
+	// open csv file
+	file, err := os.Open(*csv_path)
+	if err != nil {
+		panic("Error opening csv file")
+	}
+	defer file.Close()
+	
+	// create csv reader
+	csv_reader := csv.NewReader(file)
+
+	// attempt to read file
+	records, err := csv_reader.ReadAll()
+
+	// handle error
+	if err != nil {
+		panic("Error reading csv file")
+	}
+
+	fmt.Println(records)
+}
