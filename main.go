3dbe20da5f6fae8bc86f6a339ba81dd5eedfd1dd
TobyJWalker <tobyjwalker03@outlook.com>
2023-12-03 19:39:33 +0000

    ask question logic

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -12,6 +13,9 @@ var (
 	timer = flag.Int("t", 30, "Time limit for the quiz in seconds")
 	csv_path = flag.String("csv", "problems.csv", "Path to the csv file")
 )
+
+// create input reader
+var io = bufio.NewReader(os.Stdin)
 
 func main() {
 
@@ -36,5 +40,71 @@ func main() {
 		panic("Error reading csv file")
 	}
 
-	fmt.Println(records)
+	// create channels
+	timer_ch := make(chan interface{})
+	quiz_ch := make(chan interface{})
+	done_ch := make(chan interface{})
+
+	// prompt user to start
+	fmt.Println("Press enter to start the quiz")
+	io.ReadString('\n')
+
+	// initailise correct answers
+	correct := 0
+
+	// function to listen for correct answers
+	go func() {
+		for {
+			select {
+			case <-quiz_ch:
+				correct++
+			case <-timer_ch:
+				done_ch <- true
+			}
+		}
+	}()
+
+	// start quiz
+	go startQuiz(records, timer_ch, quiz_ch, done_ch)
+
+	
+
 }
+
+// function to start quiz
+func startQuiz(problems [][]string, timer_ch, quiz_ch, done_ch chan interface{}) {
+	
+	// track index
+	index := 0
+
+	// loop for asking questions
+	for {
+		select {
+			// handle timeout
+			case <-timer_ch:
+				fmt.Println("Time's up!")
+				return
+			
+			// ask questions
+			default:
+				// display question
+				fmt.Printf("Question %d: %s = ", index+1, problems[index][0])
+
+				// read answer
+				answer, err := io.ReadString('\n')
+				if err != nil {
+					panic("Error reading input")
+				}
+
+				// check answer
+				if answer == problems[index][1] {
+					quiz_ch <- true // signal correct answer
+				}
+
+				// increment index
+				index++
+		}
+	}
+
+}
+
